6bac3abb94cff2d9ebbebbe1405f3dd98e5d0b2e
Jakob Dilen <dilen.jakob@gmail.com>
2025-06-18 22:00:12 +0200

    Adds the possibility to change the central point of the wind model.

--- opti_implementation/advanced_wind_models.py
+++ opti_implementation/advanced_wind_models.py
@@ -1,9 +1,9 @@
 import casadi as ca
 
-def A_cosh(x, e=1):
+def A_cosh(x, e=1, central_point=2300):
     m = 1/40
     a = 2000
-    c = 2300
+    c = central_point
     d = 150
     f = 50*d / (2*e*a) * (
         ca.log(ca.cosh( ((x-(c)) + e*a) / d)) 
@@ -11,11 +11,11 @@ def A_cosh(x, e=1):
         )
     return f
 
-def B_cosh(x, e=1):
+def B_cosh(x, e=1, central_point=2300):
     a = 1150**2
     c = 1350**2
     d = 630**2
-    l = 2300
+    l = central_point
     f = -25.5 + 25.5*(d*e)  /(2*e*a) * (
         ca.log(ca.cosh( ((x - l)**2 - e*c + e*a) / (d*e))) 
         - ca.log(ca.cosh( ((x - l)**2 - e*c - e*a) / (d*e))) 
