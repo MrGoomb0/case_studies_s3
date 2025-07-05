f8519b7a370f644974f8caceb38b7cc8c3c3712f
Jakob Dilen <dilen.jakob@gmail.com>
2025-07-05 21:18:49 +0200

    Adds the final resutls.

--- step_wise_implementation/wind_models.py
+++ step_wise_implementation/wind_models.py
@@ -23,36 +23,36 @@ def Smooth(x_, x0, x1):
         x_ < x0, 0, ca.if_else(x_ > x1, 1, 6 * t**5 - 15 * t**4 + 10 * t**3)
     )
 
-def A_piecewise(x_):
-    A1 = -50 + a * x_**3 + b * x_**4
-    A2 = 0.025 * (x_ - 2300)
-    A3 = 50 - a * (4600 - x_) ** 3 - b * (4600 - x_) ** 4
-    A4 = 50
-    s1 = Smooth(x_, 480, 520)
-    s2 = Smooth(x_, 4080, 4120)
-    s3 = Smooth(x_, 4580, 4620)
-    B12 = (1 - s1) * A1 + s1 * A2
-    B23 = (1 - s2) * A2 + s2 * A3
-    B34 = (1 - s3) * A3 + s3 * A4
-    return ca.if_else(
-        x_ <= 500, B12, ca.if_else(x_ <= 4100, B23, ca.if_else(x_ <= 4600, B34, A4))
-    )
+def A_piecewise(x_, s_):
+        A1 = -50 + a * (x_/s_)**3 + b * (x_/s_)**4
+        A2 = 0.025 * ((x_/s_) - 2300)
+        A3 = 50 - a * (4600 - (x_/s_))**3 - b * (4600 - (x_/s_))**4
+        A4 = 50
+        s1 = Smooth(x_, 480 * s_, 520 * s_)
+        s2 = Smooth(x_, 4080 * s_, 4120 * s_)
+        s3 = Smooth(x_, 4580 * s_, 4620 * s_)
+        B12 = (1 - s1)*A1 + s1*A2
+        B23 = (1 - s2)*A2 + s2*A3
+        B34 = (1 - s3)*A3 + s3*A4
+        return ca.if_else(x_ <= 500 * s_, B12,
+               ca.if_else(x_ <= 4100 * s_, B23,
+               ca.if_else(x_ <= 4600 * s_, B34, A4)))
 
 
-def B_piecewise(x_):
-    B1 = d * x_**3 + e * x_**4
-    B2 = -51 * ca.exp(ca.fmin(-c * (x_ - 2300) ** 4, 30))
-    B3 = d * (4600 - x_) ** 3 + e * (4600 - x_) ** 4
-    B4 = 0
-    s1 = Smooth(x_, 480, 520)
-    s2 = Smooth(x_, 4080, 4120)
-    s3 = Smooth(x_, 4580, 4620)
-    B12 = (1 - s1) * B1 + s1 * B2
-    B23 = (1 - s2) * B2 + s2 * B3
-    B34 = (1 - s3) * B3 + s3 * B4
-    return ca.if_else(
-        x_ <= 500, B12, ca.if_else(x_ <= 4100, B23, ca.if_else(x_ <= 4600, B34, B4))
-    )
+def B_piecewise(x_, s_):
+        B1 = d * (x_/s_)**3 + e * (x_/s_)**4
+        B2 = -51 * ca.exp(ca.fmin(-c * ((x_/s_) - 2300)**4, 30))
+        B3 = d * (4600 - (x_/s_))**3 + e * (4600 - (x_/s_))**4
+        B4 = 0
+        s1 = Smooth(x_, 480 * s_, 520 * s_)
+        s2 = Smooth(x_, 4080 * s_, 4120 * s_)
+        s3 = Smooth(x_, 4580 * s_, 4620 * s_)
+        B12 = (1 - s1)*B1 + s1*B2
+        B23 = (1 - s2)*B2 + s2*B3
+        B34 = (1 - s3)*B3 + s3*B4
+        return ca.if_else(x_ <= 500 * s_, B12,
+               ca.if_else(x_ <= 4100 * s_, B23,
+               ca.if_else(x_ <= 4600 * s_, B34, B4)))
 
 def originalWindModel(x_, h_, k_):
     return wind_x(x_, k_), wind_h(x_, h_, k_)
